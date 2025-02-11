01a5e64c65f481b9ab27ce6605806df4732e66d9
Luiz Guilherme <lguidossantos29@gmail.com>
2025-02-11 16:23:38 -0300

    create apo config

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,9 +2,35 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Film from './pages/Film'
 import Header from './components/Header'
+import api from './api'
+import { useState,useEffect } from 'react'
+
 
 
 function RoutesApp() {
+    const [film,setFilm] = useState([]);
+
+    useEffect(() => {
+
+        async function loadFilmes()  {
+            const response = await api.get('movie/now_playing',{
+                params: {
+                    api_key:'956aa39c15a2fca19258140a1832df8e',
+                    language:'pt-BR',
+                    page: 1
+                }
+            })
+
+            console.log(response);
+        }
+
+        loadFilmes();
+
+        
+        
+
+    },[])
+
     return(
         <BrowserRouter>
         <Header/>
